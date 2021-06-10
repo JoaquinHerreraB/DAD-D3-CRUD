04eba0a6de3639505a2292f8c6a04e1b47ec7d7f
JoaquinHerreraB <70816183+JoaquinHerreraB@users.noreply.github.com>
2021-06-10 17:41:30 -0500

    CRUD

--- app.js
+++ app.js
@@ -1,24 +1,93 @@
 const express = require('express');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'usuarios_db',
+    user:'root',
+    password: 'root'
+});
+var jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({extended: false});
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+conexion.connect(function(error){
+    if (error){
+        throw error;
+    }else{
+        console.log('Conexion exitosa')
+    }
+})
 app.get('/', urlencodedParser, (req, res) =>{
-    /* let url = req.url;
-    url = url.substring(url.indexOf('?') + 1);
-
-    const params = url.split('&');
-    let text = '';
-    
-    params.forEach(param => {
-        let object = param.split('=');
-        text += object[0] + ' : ' + object[1] + '<br/>'; 
-    }); */
+    var nombre = req.query.nombre;
+    var apellido = req.query.apellido;
     res.send(`<h1>Tus datos son: <br/>
-    Nombre: ${req.query.nombre} <br/>
-    Apellido: ${req.query.apellido} </h1>`);
+    Nombre: ${nombre} <br/>
+    Apellido: ${apellido} </h1>`);
+    const data = req.query;
+    conexion.query('INSERT INTO datos set ?',[data], (error, rows)=>{
+        console.log(rows);
+    })
+    console.log(req.query);
+});
+
+app.get('/list', urlencodedParser, (req, res) =>{
+    conexion.query('SELECT * FROM datos', function(error, resultados, campos){
+        if (error){
+            throw error;
+        }
+        var valores = "";
+        resultados.forEach(resultado => {
+           valores += JSON.stringify(resultado);
+            console.log(resultado);
+        });
+        res.send(`<h1>Los datos son: <br/>
+        ${valores} <br/></h1>`)
+    })
+});
+
+app.post('/insert', urlencodedParser, (req, res) =>{
+    var id = req.body.id;
+    var nombre = req.body.nombre;
+    var apellido = req.body.apellido;
+    res.send(`<h1>Los datos insertados son: <br/>
+    Codigo: ${id} <br/>
+    Nombre: ${nombre} <br/>
+    Apellido: ${apellido} </h1>`);
+    const data = req.body;
+    conexion.query('INSERT INTO datos set ?',[data], (error, rows)=>{
+        console.log(rows);
+    })
+    console.log(req.body);
+});
+app.post('/update', urlencodedParser, (req, res) =>{
+    var id = req.body.id;
+    var nombre = req.body.nombre;
+    var apellido = req.body.apellido;
+    res.send(`<h1>Los datos actualizados son son: <br/>
+    Codigo: ${id} <br/>
+    Nombre: ${nombre} <br/>
+    Apellido: ${apellido} </h1>`);
+    var form_data = {
+        nombre: nombre,
+        apellido: apellido
+    }
+    conexion.query('UPDATE datos SET ? WHERE id = ' + id, form_data, function(err, rows){
+        console.log(rows);
+    })
+    console.log(req.body);
+});
+app.get('/delete/', urlencodedParser, (req, res) =>{
+    var id = req.query.id;
+    res.send(`<h1>Se ha eliminado a alumno de código: <br/>
+    Código: ${id} <br/> </h1>`);
+    const data = req.query;
+    
+    conexion.query('DELETE FROM datos WHERE id = '+ id , (error, rows)=>{
+        console.log(rows);
+    })
     console.log(req.query);
 });
 
